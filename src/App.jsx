30c8af3328feb591fea5409259ce2d7e564c2c98
Karen Paguada <135404251+karen8819@users.noreply.github.com>
2023-08-17 06:49:39 -0600

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+import TodoList from './components/TodoList'
+import './App.css'
+
+function App() {
+  
+
+  return (
+   
+    <div>
+      <div className="App">
+      <h1 ><em>Lista de Tareas</em></h1>
+      <TodoList />
+    </div>
+      
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/TodoList.jsx
@@ -0,0 +1,65 @@
+import { useEffect,  useState} from 'react';
+import TodoItem from './TodoItem';
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState('');
+  const [tasks, setTasks] =useState('');
+
+  useEffect(()=>{
+    const storedTodos=JSON.parse(localStorage.getItem('todos')) || [];
+    setTodos(storedTodos);
+  }, []);
+
+useEffect(()=>{
+    localStorage.setItem('todos', JSON.stringify(todos));
+}, [todos]);
+  
+  useEffect(() => {
+  }, []);
+
+  const addTodo = () => {
+    if (inputText.trim() !== '') {
+      setTodos([...todos, { text: inputText, completed: false }]);
+      setInputText('');
+    }
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = todos.map((todo, i) =>
+      i === index ? { ...todo, completed: !todo.completed } : todo
+    );
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className='contenedor'>
+      <div className='contenedorTareas'>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
+        <button onClick={addTodo}>Agregar</button>
+      </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <TodoItem
+            key={index}
+            index={index}
+            todo={todo}
+            deleteTodo={deleteTodo}
+            toggleTodo={toggleTodo}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoList;
