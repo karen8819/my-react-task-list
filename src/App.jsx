cea4f8d919fb314753a553a5d7f17793b7a60f6a
Karen Paguada <135404251+karen8819@users.noreply.github.com>
2023-09-12 10:45:05 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,48 @@
 
-import React from 'react'
-import TodoList from './components/TodoList'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import TareaForm from './components/TareaForm';
+import ListaTareas from './components/ListaTareas';
 
 function App() {
+  const [tareas, setTareas] = useState(() => {
+    const savedTareas = localStorage.getItem('tareas');
+    return savedTareas ? JSON.parse(savedTareas) : [];
+  });
   
+  useEffect(() => {
+    localStorage.setItem('tareas', JSON.stringify(tareas));
+  }, [tareas]);
+
+  const agregarTarea = (nuevaTarea) => {
+    setTareas([...tareas, nuevaTarea]);
+  };
+
+  const seleccionarTarea = (index) => {
+    const nuevasTareas = [...tareas];
+    nuevasTareas[index].completada = !nuevasTareas[index].completada;
+    setTareas(nuevasTareas);
+  };
+
+  const eliminarTarea = (index) => {
+    if (window.confirm('¿Seguro que quieres eliminar esta tarea?')) {
+      const nuevasTareas = [...tareas];
+      nuevasTareas.splice(index, 1);
+      setTareas(nuevasTareas);
+  }
+  };
 
   return (
-   
-    <div>
-      <div className="App">
-      <h1 ><em>Lista de Tareas</em></h1>
-      <TodoList />
+    <div className="App">
+      <h1><em>Lista de Tareas</em></h1>
+      <TareaForm agregarTarea={agregarTarea} />
+      <ListaTareas
+        tareas={tareas}
+        seleccionarTarea={seleccionarTarea}
+        eliminarTarea={eliminarTarea}
+      />
     </div>
-      
-    </div>
-    
-  )
+  );
 }
 
-export default App
+export default App;
